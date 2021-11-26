58342dc790218b28e66ded3948bb5ee084fcc556
Mukhin Artem <artemmuha@gmail.com>
2021-11-26 11:49:46 +0400

    feat(License): add MIT license feat(setup.py): adding additional info in order to address pip packaging

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup
 import setuptools
 
+with open("Readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='HSI_Dataset_API',
     packages=setuptools.find_packages(),
-    version='1.4.1',
-    description='...',
-    long_description='...',
+    version='1.4.2',
+    description='API for accessing HSI datasets',
+    long_description=long_description,
     author='Mukhin Artem',
     author_email='artemmukhinssau@gmail.com',
     url='https://gitlab.com/rustam-industries/hsi_dataset_api',
     include_package_data=True,
     classifiers=[
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ], install_requires=[]
 )
